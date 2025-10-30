51a78e8c9fd48c083514287d612e50ac3fe35a34
Arnold <aovasquez31@ucatolica.edu.co>
2025-10-29 22:55:54 -0500

    Advertencias de imports necesarias en database.py (# noqa: F401)

--- database.py
+++ database.py
@@ -1,3 +1,10 @@
+"""
+Módulo: database
+Autor: Breiner Vásquez
+Descripción:
+Configura la conexión a la base de datos SQLite y gestiona sesiones de SQLModel.
+"""
+
 from sqlmodel import SQLModel, create_engine, Session
 
 sqlite_file_name = "database.db"
@@ -6,10 +13,16 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
 def create_db_and_tables():
-    # base de datos."""
+    """Crea las tablas en la base de datos."""
+
+    # 👇 Importamos los modelos para registrar las tablas
+    from models.empleado import Empleado  # noqa: F401
+    from models.proyecto import Proyecto  # noqa: F401
+    from models.relacion import ProyectoEmpleadoLink  # noqa: F401
+
     SQLModel.metadata.create_all(engine)
 
 def get_session():
-    #sesión de base de datos
+    """Proporciona una sesión de base de datos para las operaciones."""
     with Session(engine) as session:
         yield session
